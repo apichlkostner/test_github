a4b9e485580e9e41f87d40be6a1ddbce3feb2e1f
Arthur Pichlkostner <apichlkostner@gmx.de>
2025-11-14 15:54:06 +0100

    Function reopen added

--- src/filesystem.h
+++ src/filesystem.h
@@ -2,6 +2,7 @@
 
 #include <span>
 #include <stdint.h>
+#include <string>
 #include <string_view>
 #include <vector>
 
@@ -10,10 +11,12 @@ class Filesystem
 public:
     ~Filesystem();
     bool open(std::string_view filename);
+    bool reopen();
     void close();
     bool write(std::span<const uint8_t> data);
     bool read(std::vector<uint8_t> &data);
 
 private:
     int fd_{-1};
+    std::string filename_{};
 };
